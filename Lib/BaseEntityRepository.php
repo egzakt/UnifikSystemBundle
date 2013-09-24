8b920479f21f200659f5d0d5f4c4a7bc0970326a
Jean-François Côté <tioiscote@gmail.com>
2013-09-24 15:48:21 -0400

    Bring back the BaseEntityRepository, having only the processQuery stuff Repositories extend the BaseEntityRepository, Translatable Repo uses the TranslatableEntityRepository trait.

---
+++ Lib/BaseEntityRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Egzakt\SystemBundle\Lib;
+
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Egzakt Backend Base for Entities
+ */
+abstract class BaseEntityRepository extends EntityRepository implements ContainerAwareInterface
+{
+    /**
+     * Dependency injection container
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var boolean $returnQueryBuilder
+     */
+    private $returnQueryBuilder;
+
+    /**
+     * Sets the Container
+     *
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Set ReturnQueryBuilder
+     *
+     * @param bool $returnQueryBuilder
+     */
+    public function setReturnQueryBuilder($returnQueryBuilder)
+    {
+        $this->returnQueryBuilder = $returnQueryBuilder;
+    }
+
+    /**
+     * Get ReturnQueryBuilder
+     *
+     * @return bool
+     */
+    public function getReturnQueryBuilder()
+    {
+        return $this->returnQueryBuilder;
+    }
+
+    /**
+     * Returns the Query Builder or the results depending on the repository parameters
+     *
+     * @param \Doctrine\ORM\QueryBuilder $queryBuilder
+     * @param bool                       $singleResult
+     *
+     * @return mixed
+     */
+    protected function processQuery($queryBuilder, $singleResult = false)
+    {
+        if ($this->returnQueryBuilder) {
+            return $queryBuilder;
+        }
+
+        if ($singleResult) {
+            return $queryBuilder->getQuery()->getSingleResult();
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+}
