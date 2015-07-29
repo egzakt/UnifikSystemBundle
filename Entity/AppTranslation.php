37c9af45088a714942bc0c4715f1fd4b27139584
Naomi <n.denoncourt@egzakt.com>
2015-07-29 11:12:39 -0400

    Adjustments for multiapp Added translation for App entity + app "slug" was replaced by "code" (which must match an application core) because slug now refers to a real translatable slug (in the frontend url)

---
+++ Entity/AppTranslation.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Unifik\SystemBundle\Entity;
+
+use Unifik\DoctrineBehaviorsBundle\Model as UnifikORMBehaviors;
+
+/**
+ * SectionTranslation
+ */
+class SectionTranslation
+{
+    use UnifikORMBehaviors\Translatable\Translation;
+    use UnifikORMBehaviors\Sluggable\Sluggable;
+    use UnifikORMBehaviors\Metadatable\Metadatable;
+    use UnifikORMBehaviors\Taggable\Taggable;
+
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var boolean $active
+     */
+    private $active;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * Get active
+     *
+     * @return boolean
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * Get Sluggable Fields
+     *
+     * @return array
+     */
+    public function getSluggableFields()
+    {
+        return array('name');
+    }
+
+}
